ad362495da1276b2cde0e31a63080f2b38da600e
RingoftheKing <90739493+RingoftheKing@users.noreply.github.com>
2025-09-05 12:54:31 +0800

    Fix Login and Register jests
    Part of Lab 2 jest tutorial

--- client/src/pages/Auth/Register.test.js
+++ client/src/pages/Auth/Register.test.js
@@ -14,13 +14,17 @@ jest.mock('../../context/auth', () => ({
     useAuth: jest.fn(() => [null, jest.fn()]) // Mock useAuth hook to return null state and a mock function for setAuth
   }));
 
-  jest.mock('../../context/cart', () => ({
+jest.mock('../../context/cart', () => ({
     useCart: jest.fn(() => [null, jest.fn()]) // Mock useCart hook to return null state and a mock function
   }));
     
 jest.mock('../../context/search', () => ({
     useSearch: jest.fn(() => [{ keyword: '' }, jest.fn()]) // Mock useSearch hook to return null state and a mock function
   }));  
+
+jest.mock('../../hooks/useCategory', () => 
+  jest.fn(() => [])
+);
 
   Object.defineProperty(window, 'localStorage', {
     value: {
@@ -47,6 +51,7 @@ describe('Register Component', () => {
 
   it('should register the user successfully', async () => {
     axios.post.mockResolvedValueOnce({ data: { success: true } });
+    axios.get.mockResolvedValueOnce({ data: { success: true, message: "All Categories List"} });
 
     const { getByText, getByPlaceholderText } = render(
         <MemoryRouter initialEntries={['/register']}>
