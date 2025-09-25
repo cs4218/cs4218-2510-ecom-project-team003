75d68f34f8af61346fe2ae0193cf325585499057
Samuel Lim <samuelim01@gmail.com>
2025-09-25 14:16:30 +0800

    Add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig, globalIgnores } from "eslint/config";
+import jest from 'eslint-plugin-jest';
+import importPlugin from 'eslint-plugin-import';
+
+export default defineConfig([
+  globalIgnores(['client', 'node_modules', 'playwright-report', 'coverage']),
+  { 
+    files: ["**/*.js"], 
+    plugins: { js }, 
+    extends: [js.configs.recommended, importPlugin.flatConfigs.recommended], 
+    languageOptions: { 
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      } 
+    },
+  },
+  {
+    files: ["**/*.test.js"],
+    ...jest.configs['flat/recommended'],
+  }
+]);
