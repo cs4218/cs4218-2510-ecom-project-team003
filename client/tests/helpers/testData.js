3122b91783569ee2b3991ac431e2dd625832084d
Samuel Lim <samuelim01@gmail.com>
2025-10-08 22:58:38 +0800

    Add productDetails integration tests

---
+++ client/tests/helpers/testData.js
@@ -0,0 +1,32 @@
+export const ELECTRONICS = {
+  _id: '68e3f943282387623f0a0736',
+  name: 'Electronics',
+  slug: 'electronics',
+}
+
+export const LAPTOP = {
+  name: 'Laptop',
+  slug: 'laptop',
+  description: 'A powerful laptop',
+  price: 1499.99,
+  category: ELECTRONICS._id,
+  quantity: 30,
+};
+
+export const SMARTPHONE = {
+  name: 'Smartphone',
+  slug: 'smartphone',
+  description: 'A high-end smartphone',
+  price: 999.99,
+  category: ELECTRONICS._id,
+  quantity: 50,
+};
+
+export const TABLET = {
+  name: 'Tablet',
+  slug: 'tablet',
+  description: 'A sleek tablet with high performance',
+  price: 599.99,
+  category: ELECTRONICS._id,
+  quantity: 40,
+};
