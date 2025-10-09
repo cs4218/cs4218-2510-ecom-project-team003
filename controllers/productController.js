a0182b74b51013cf6f86d88bbc89c3b84dac9914
samuelim01 <61283948+samuelim01@users.noreply.github.com>
2025-10-09 21:28:32 +0800

    Set up frontend integration framework (#52)
    * Set up frontend integration framework
    * Fix braintree failing on integration tests
    * Remove windows workflow

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,22 +1,9 @@
 import productModel from "../models/productModel.js";
 import categoryModel from "../models/categoryModel.js";
-import orderModel from "../models/orderModel.js";
 
 import fs from "fs";
 import slugify from "slugify";
-import braintree from "braintree";
-import dotenv from "dotenv";
 import * as z from "zod";
-
-dotenv.config();
-
-//payment gateway
-var gateway = new braintree.BraintreeGateway({
-  environment: braintree.Environment.Sandbox,
-  merchantId: process.env.BRAINTREE_MERCHANT_ID,
-  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
-  privateKey: process.env.BRAINTREE_PRIVATE_KEY,
-});
 
 export const productFiltersSchema = z.object({
   checked: z.array(z.string()).default([]),
