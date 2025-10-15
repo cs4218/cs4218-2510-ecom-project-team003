9ccbaf939dfa4b209c276c7ce4c21ef60365dca9
Samuel Lim <samuelim01@gmail.com>
2025-10-16 00:30:06 +0800

    Fix AdminRoute
    * Prevent premature logout
    * Remove Loader
    * Update AdminRoute and Spinner tests accordingly

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-const Spinner = ({ path = "login" }) => {
+const Spinner = ({ path = "login", onTimeout }) => {
   const [count, setCount] = useState(3);
   const navigate = useNavigate();
   const location = useLocation();
 
   useEffect(() => {
+    if (count === 0) {
+      if (typeof onTimeout === "function") onTimeout();
+      navigate(`/${path}`, { state: location.pathname });
+      return;
+    }
+
     const interval = setInterval(() => {
       setCount((prevValue) => --prevValue);
     }, 1000);
-    count === 0 && navigate(`/${path}`, {
-        state: location.pathname,
-      });
+
     return () => clearInterval(interval);
   }, [count, navigate, location]);
   return (
