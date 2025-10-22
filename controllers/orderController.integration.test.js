3b620f15f241d1936ab3f954c1953a5ba74f35b2
Samuel Lim <samuelim01@gmail.com>
2025-10-22 10:46:32 +0800

    Fix stateful tests

--- controllers/orderController.integration.test.js
+++ controllers/orderController.integration.test.js
@@ -11,21 +11,19 @@ import {
     USER, ADMIN,
     ORDER_TWO_ITEMS_PROCESSING,
 } from "../client/tests/helpers/testData";
-import {describe} from "node:test";
-import {expect} from "@jest/globals";
 
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-dotenv.config({"path": ".env"});
+dotenv.config({ "path": ".env" });
 
 const admin_token = jwt.sign(
-    { _id: ADMIN._id},
+    { _id: ADMIN._id },
     process.env.JWT_SECRET,
     { expiresIn: "1h" }
 );
 
 const user_token = jwt.sign(
-    { _id: USER._id},
+    { _id: USER._id },
     process.env.JWT_SECRET,
     { expiresIn: "1h" }
 );
@@ -33,22 +31,26 @@ const user_token = jwt.sign(
 describe("Order Controller Integration Tests", () => {
     beforeAll(async () => {
         await createAndConnectTestDB();
+    });
+
+    afterEach(async () => {
         await clearTestDB();
-        
-        await userModel.insertOne(ADMIN);
-        await userModel.insertOne(USER);
-        await categoryModel.insertMany([ELECTRONICS, BOOKS, CLOTHING]);
-        await productModel.insertMany([SMARTPHONE, TABLET, STUDY_GUIDE, LONG_DESC_PRODUCT, LAPTOP])
-        await orderModel.insertOne(ORDER_TWO_ITEMS_PROCESSING);
-    })
+    });
 
     afterAll(async () => {
-        await clearTestDB();
         await closeTestDB();
-    })
+    });
 
     describe("getOrders tests", () => {
+        beforeEach(async () => {
+            await userModel.insertOne(ADMIN);
+            await userModel.insertOne(USER);
+            await categoryModel.insertMany([ELECTRONICS, BOOKS, CLOTHING]);
+            await productModel.insertMany([SMARTPHONE, TABLET, STUDY_GUIDE, LONG_DESC_PRODUCT, LAPTOP])
+        });
+
         it("should return orders for the logged-in user", async () => {
+            await orderModel.insertOne(ORDER_TWO_ITEMS_PROCESSING);
             const response = await request(app)
                 .get("/api/v1/order/orders")
                 .set("Authorization", `Bearer ${user_token}`)
@@ -93,7 +95,7 @@ describe("Order Controller Integration Tests", () => {
         });
 
         it("should handle unexpected errors gracefully", async () => {
-            const logSpy = jest.spyOn(console, "log").mockImplementation(() => {});
+            const logSpy = jest.spyOn(console, "log").mockImplementation(() => { });
             jest.spyOn(orderModel, "find").mockImplementationOnce(() => {
                 throw new Error("Database failure");
             });
@@ -113,6 +115,14 @@ describe("Order Controller Integration Tests", () => {
     })
 
     describe("getAllOrders tests", () => {
+        beforeEach(async () => {
+            await userModel.insertOne(ADMIN);
+            await userModel.insertOne(USER);
+            await categoryModel.insertMany([ELECTRONICS, BOOKS, CLOTHING]);
+            await productModel.insertMany([SMARTPHONE, TABLET, STUDY_GUIDE, LONG_DESC_PRODUCT, LAPTOP])
+            await orderModel.insertOne(ORDER_TWO_ITEMS_PROCESSING);
+        });
+
         it("should return 500 for unexpected error", async () => {
             // Mock find() to simulate DB failure
             jest.spyOn(orderModel, "find").mockImplementationOnce(() => {
@@ -159,6 +169,14 @@ describe("Order Controller Integration Tests", () => {
     });
 
     describe("updateOrderStatus tests", () => {
+        beforeEach(async () => {
+            await userModel.insertOne(ADMIN);
+            await userModel.insertOne(USER);
+            await categoryModel.insertMany([ELECTRONICS, BOOKS, CLOTHING]);
+            await productModel.insertMany([SMARTPHONE, TABLET, STUDY_GUIDE, LONG_DESC_PRODUCT, LAPTOP])
+            await orderModel.insertOne(ORDER_TWO_ITEMS_PROCESSING);
+        });
+
         it("should return 500 for unexpected error", async () => {
             // Mock find() to simulate DB failure
             jest.spyOn(orderModel, "findByIdAndUpdate").mockImplementationOnce(() => {
